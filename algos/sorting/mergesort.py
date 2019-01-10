9f79718ebafe59e5a70a51e458dfc311ba9b04e4
rikithreddy <rikith.legend@gmail.com>
2019-01-10 14:36:45 +0530

    merge sort completed

--- algos/sorting/mergesort.py
+++ algos/sorting/mergesort.py
@@ -1,13 +1,37 @@
-numbers = map(int, input().split())
+numbers = list(map(int, input().split()))
+
 
 def mergesort(a):
-	if len==1:
+	if len(a)==1:
 		return a
 	else:
-		return merge(a[0:int(len(a)/2)], a[int(len(a)/2):len(a)])
+		mid = int(len(a)/2)
+		return merge(a[0:mid], a[mid:len(a)])
 
 def merge(a,b):
 	a = mergesort(a)
 	b = mergesort(b)
+	i=0
+	j=0
 
-	for
+	merged = []
+	while i< len(a) and j < len(b):
+		if a[i] < b[j]:
+			merged.append(a[i])
+			i+=1
+		else:
+			merged.append(b[j])
+			j+=1
+
+	while i < len(a):
+		merged.append(a[i])
+		i+=1
+
+
+	while j < len(b):
+		merged.append(b[j])
+		j+=1
+	return merged
+	
+x = mergesort(numbers)
+print (*x )
