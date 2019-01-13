0acbabe6c32feb5d20343076f50b98f4b92d7b62
Rikith <rikith.legend@gmail.com>
2019-01-13 12:09:15 +0530

    solved codechef CMB01

---
+++ codechef/peer/solved/CMB01.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for _ in range(t):
+	x,y = input().split()
+	x =x.strip()
+	y = y.strip()
+	x = x[::-1] 
+	y = y[::-1]
+
+	x = int(x)
+	y = int(y)
+	answer = (x+y)
+	strans = str(answer)[::-1]
+	while len(strans)!=1:
+		if strans[0]=='0':
+			strans = strans[1:]
+		else:
+			break 
+	print (strans)
